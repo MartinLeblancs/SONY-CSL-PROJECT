7187e3df019e717992996a958648e714c09d3d2b
Sandro SGRO <sandro.sgro@epitech.eu>
2022-05-25 15:08:14 +0200

    [MOD] Components (all components necessary)

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
+import React, {Component} from "react"
 
-function App() {
-  return (
-    <div className="App" style={{
-      backgroundImage: "url(/background.jpg)",
-      width: '100vw',
-      height: '100vh',
-      backgroundRepeat: "no-repeat",
-      backgroundSize: 'cover'
+class App extends Component {
+  render() {
+    return (
+      <div className="App" style={{
+        backgroundImage: "url(/background.jpg)",
+        width: '100vw',
+        height: '100vh',
+        // margin: 0,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: 'cover'
       }}>
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload (test).
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload (test).
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        {/* <body>
+        </body> */}
+      </div>
+    )
+  }
 }
 
 export default App;
