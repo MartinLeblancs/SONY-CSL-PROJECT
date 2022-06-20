aa0b0c62040e7efe2aeb6bac05c15559ef44851f
ReyDen-Martin <martin.leblancs@epitech.eu>
2022-06-20 14:08:23 +0200

    [ADD] Testing fetch

--- src/components/MusicApi.js
+++ src/components/MusicApi.js
@@ -1,13 +1,63 @@
-import React, { Component } from 'react';
- 
-class MusicApi extends Component {
-  render() {
-    return (
-      <div>
-        This is MusicApi component.
-      </div>
-    );
-  }
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+const e = React.createElement;
+
+class MusicApi extends React.Component {
+    async componentDidMount() {
+        const requestOptions = {
+            method: 'POST',
+            headers: {"Content-type": "application/json; charset=UTF-8"},
+            body: JSON.stringify({
+                "id": 0,
+                "clip_id": 0,
+                "detail_clip_id": 0,
+                "note_density": 0.1,
+                "tempo": 120,
+                "case": "start",
+                "top_p": 0.949999988079071,
+                "superconditioning": 1,
+                "clip_start": 0.0,
+                "clip_end": 5,
+                "selected_region": {
+                "start": 1,
+                    "end": 2
+            },
+            "notes": [
+                "notes",
+                1,
+                "note",
+                76,
+                0.0,
+                0.0,
+                120,
+                0
+            ]
+        }
+        )
+        };
+        fetch('https://pia.api.cslmusic.team/', requestOptions)
+            .then((response) => {
+                return response.json()
+            })
+            .then((data) => {
+                data.render()
+
+                // Work with JSON data here
+                console.log(data)
+            })
+            .catch((err) => {
+
+                console.log('An error has occured with fetch\n' + err)
+            })
+    }
+
+    render() {
+        return (
+            <h1>TESTING FETCH()</h1>
+        );
+    }
 }
- 
-export default MusicApi;
+
+const domContainer = document.querySelector('#MusicApi');
+ReactDOM.render(e(MusicApi), domContainer);
