aa5ab104784f64da03184b038a53a8f3c5de7bfb
Sandro SGRO <sandro.sgro@epitech.eu>
2022-06-30 11:15:06 +0200

    [MOD] add options to PianoRoll (bpm, noteFormat, zoom, resolution)

--- src/App.js
+++ src/App.js
@@ -66,29 +66,32 @@ export default function App() {
     }
   });
   return (
-      <>
-      <MusicApi></MusicApi>
-        <div className="App">
-      <PianoRoll
-        width={810}
-        height={400}
-        zoom={6}
-        // resolution={2}
-        gridLineColor={0x333333}
-        blackGridBgColor={0x1e1e1e}
-        whiteGridBgColor={0x282828}
-        noteData={[
-          ["0:0:0", "C4", "2n"],
-          ["0:0:0", "D4", "2n"],
-          ["0:0:0", "E4", "2n"],
-          ["0:2:0", "B4", "4n"],
-          ["0:3:0", "A#4", "4n"]
-        ]}
-        ref={playbackRef}
-      />
-      <button onClick={() => setState(state + 1)}>set state</button>
-    </div>
-      </>
-
+    <>
+    {/* <MusicApi></MusicApi> */}
+      <div className="App">
+        <PianoRoll
+          width={1000}
+          height={600}
+          zoom={5}
+          bpm={120}
+          resolution={2}
+          gridLineColor={0x333333}
+          blackGridBgColor={0x1e1e1e}
+          whiteGridBgColor={0x282828}
+          noteFormat={"String"}
+          noteData={[
+            ["0:0:0", "B1", ""],
+            ["0:2:0", "F5", ""],
+            ["0:0:0", "D4", "2n"],
+            ["0:0:0", "E4", "2n"],
+            ["0:2:0", "B4", "4n"],
+            ["0:3:0", "A#4", "4n"],
+            ["0:2:0", "B4", "4n"]
+          ]}
+          ref={playbackRef}
+        />
+        {/* <button onClick={() => setState(state + 1)}>set state</button> */}
+      </div>
+    </>
   )
 }
