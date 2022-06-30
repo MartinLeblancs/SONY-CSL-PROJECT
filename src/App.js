bbb17009018b4beca2de8a56ed825db0eb258654
Reyden <martin.leblancs@epitech.eu>
2022-06-30 12:47:45 +0200

    [ADD] Dropdown menu in main page

--- src/App.js
+++ src/App.js
@@ -48,50 +48,49 @@
 //
 // export default App;
 
-// import "./App.css";
-// import { useRef, useState } from "react";
-// import PianoRoll from "react-piano-roll";
-// import {MusicApi} from "./components/MusicApi"; 
+import "./App.css";
+import { useRef, useState } from "react";
+import PianoRoll from "react-piano-roll";
+import {MusicApi} from "./components/MusicApi";
 
-// export default function App() {
-//   const [state, setState] = useState(0);
-//   const playbackRef = useRef();
-//   window.addEventListener("keydown", ({ key }) => {
-//     if (playbackRef.current === undefined) {
-//       return;
-//     } else if (key === " ") {
-//       playbackRef.current.toggle();
-//     } else if (key === "Enter") {
-//       playbackRef.current.seek("0:0:0");
-//     }
-//   });
-//   return (
-//     <>
-//     {/* <MusicApi></MusicApi> */}
-//       <div className="App">
-//         <PianoRoll
-//           width={1000}
-//           height={600}
-//           zoom={5}
-//           bpm={120}
-//           resolution={2}
-//           gridLineColor={0x333333}
-//           blackGridBgColor={0x1e1e1e}
-//           whiteGridBgColor={0x282828}
-//           noteFormat={"String"}
-//           noteData={[
-//             ["0:0:0", "B1", ""],
-//             ["0:2:0", "F5", ""],
-//             ["0:0:0", "D4", "2n"],
-//             ["0:0:0", "E4", "2n"],
-//             ["0:2:0", "B4", "4n"],
-//             ["0:3:0", "A#4", "4n"],
-//             ["0:2:0", "B4", "4n"]
-//           ]}
-//           ref={playbackRef}
-//         />
-//         {/* <button onClick={() => setState(state + 1)}>set state</button> */}
-//       </div>
-//     </>
-//   )
-// }
+export default function App() {
+  const [state, setState] = useState(0);
+  const playbackRef = useRef();
+  window.addEventListener("keydown", ({ key }) => {
+    if (playbackRef.current === undefined) {
+      return;
+    } else if (key === " ") {
+      playbackRef.current.toggle();
+    } else if (key === "Enter") {
+      playbackRef.current.seek("0:0:0");
+    }
+  });
+  return (
+    <>
+    {/*  <div className="App">*/}
+    {/*    <PianoRoll*/}
+    {/*      width={1000}*/}
+    {/*      height={600}*/}
+    {/*      zoom={5}*/}
+    {/*      bpm={120}*/}
+    {/*      resolution={2}*/}
+    {/*      gridLineColor={0x333333}*/}
+    {/*      blackGridBgColor={0x1e1e1e}*/}
+    {/*      whiteGridBgColor={0x282828}*/}
+    {/*      noteFormat={"String"}*/}
+    {/*      noteData={[*/}
+    {/*        ["0:0:0", "B1", ""],*/}
+    {/*        ["0:2:0", "F5", ""],*/}
+    {/*        ["0:0:0", "D4", "2n"],*/}
+    {/*        ["0:0:0", "E4", "2n"],*/}
+    {/*        ["0:2:0", "B4", "4n"],*/}
+    {/*        ["0:3:0", "A#4", "4n"],*/}
+    {/*        ["0:2:0", "B4", "4n"]*/}
+    {/*      ]}*/}
+    {/*      ref={playbackRef}*/}
+    {/*    />*/}
+    {/*    /!* <button onClick={() => setState(state + 1)}>set state</button> *!/*/}
+    {/*  </div>*/}
+    </>
+  )
+}
--- src/components/MusicList.js
+++ src/components/MusicList.js
@@ -1,13 +1,79 @@
-import React, { Component } from 'react';
- 
+import React, { Component, useState  } from 'react';
+import {FormControl, Dropdown, Navbar, NavDropdown, Nav, Container} from "react-bootstrap";
+// import {DropdownButton} from "react-bootstrap";
+
+const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
+    <a
+        href=""
+        ref={ref}
+        onClick={(e) => {
+            e.preventDefault();
+            onClick(e);
+        }}
+    >
+        {children}
+        &#x25bc;
+    </a>
+));
+
+// forwardRef again here!
+// Dropdown needs access to the DOM of the Menu to measure it
+const CustomMenu = React.forwardRef(
+    ({ children, style, className, 'aria-labelledby': labeledBy }, ref) => {
+        const [value, setValue] = useState('');
+
+        return (
+            <div
+                ref={ref}
+                style={style}
+                className={className}
+                aria-labelledby={labeledBy}
+            >
+                <FormControl
+                    autoFocus
+                    className="mx-3 my-2 w-auto"
+                    placeholder="Type to filter..."
+                    onChange={(e) => setValue(e.target.value)}
+                    value={value}
+                />
+                <ul className="list-unstyled">
+                    {React.Children.toArray(children).filter(
+                        (child) =>
+                            !value || child.props.children.toLowerCase().startsWith(value),
+                    )}
+                </ul>
+            </div>
+        );
+    },
+);
+
 class MusicList extends Component {
+
   render() {
     return (
-      <div>
-        This is MusicList component.
-      </div>
+        <Navbar variant="dark" bg="dark" expand="lg">
+            <Container fluid>
+                <Navbar.Brand href="#home">MUSIQUE A TROUS</Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbar-dark-example" />
+                <Navbar.Collapse id="navbar-dark-example">
+                    <Nav>
+                        <NavDropdown
+                            id="nav-dropdown-dark-example"
+                            title="Musiques"
+                            menuVariant="dark"
+                        >
+                            <NavDropdown.Item href="#action/3.1">TEST1</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">TEST2</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">TEST3</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">TEST4</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
   }
 }
- 
+
 export default MusicList;
