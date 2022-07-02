86f9231f28cd9add6bb95303c92a319f1fdcec3e
Reyden <martin.leblancs@epitech.eu>
2022-07-02 12:52:21 +0200

    [ADD] Change Buttons input Start timer and End timer

--- src/components/ButtonsTimer.js
+++ src/components/ButtonsTimer.js
@@ -1,69 +1,68 @@
 import * as React from 'react';
+import TimeField from 'react-simple-timefield';
 import './ButtonsTimer.css'
+import {TextField} from "@mui/material";
 
-const TimeInput = () => {
-    const [value, setValue] = React.useState("0:00");
+class ButtonsTimer extends React.Component{
+    constructor() {
+        super();
 
-    const onChange = (event) => {
-        setValue(event.target.value);
-    };
+        this.state = {
+            time: '12:34',
+            timeSeconds: '12:34:56',
+            timeSecondsCustomColon: '12-34-56'
+        };
 
-    const onBlur = (event) => {
-        const value = event.target.value;
-        const seconds = Math.max(0, getSecondsFromHHMMSS(value));
+        this.onTimeChange = this.onTimeChange.bind(this);
+    }
 
-        const time = toHHMMSS(seconds);
-        setValue(time);
-    };
+    onTimeChange(event, value) {
+        const newTime = value.replace(/-/g, ':');
+        const time = newTime.substr(0, 5);
+        const timeSeconds = newTime.padEnd(8, this.state.timeSeconds.substr(5, 3));
+        const timeSecondsCustomColon = timeSeconds.replace(/:/g, '-');
 
-    const getSecondsFromHHMMSS = (value) => {
-        const [str1, str2, str3] = value.split(":");
+        this.setState({time, timeSeconds, timeSecondsCustomColon});
+    }
+    render() {
+        const {start, end} = this.state;
+        return (
+            <section className="container">
+                <h2>START:</h2>
+                <section>
+                    <TimeField
+                        showSeconds
+                        value={start}
+                        onChange={this.onTimeChange}
+                        style={{
+                            border: '2px solid #666',
+                            fontSize: 42,
+                            width: 180,
+                            padding: '5px 8px',
+                            color: '#333',
+                            borderRadius: 3
+                        }}
+                    />
+                </section>
+                <h2>END:</h2>
+                <section>
+                    <TimeField
+                        showSeconds
+                        value={end}
+                        onChange={this.onTimeChange}
+                        style={{
+                            border: '2px solid #666',
+                            fontSize: 42,
+                            width: 180,
+                            padding: '5px 8px',
+                            color: '#333',
+                            borderRadius: 3
+                        }}
+                    />
+                </section>
+            </section>);
 
-        const val1 = Number(str1);
-        const val2 = Number(str2);
-        const val3 = Number(str3);
-
-        if (!isNaN(val1) && isNaN(val2) && isNaN(val3)) {
-            return val1;
-        }
-
-        if (!isNaN(val1) && !isNaN(val2) && isNaN(val3)) {
-            return val1 * 60 + val2;
-        }
-
-        if (!isNaN(val1) && !isNaN(val2) && !isNaN(val3)) {
-            return val1 * 60 * 60 + val2 * 60 + val3;
-        }
-
-        return 0;
-    };
-
-    const toHHMMSS = (secs) => {
-        const secNum = parseInt(secs.toString(), 10);
-        const hours = Math.floor(secNum / 3600);
-        const minutes = Math.floor(secNum / 60) % 60;
-        const seconds = secNum % 60;
-
-        return [hours, minutes, seconds]
-            .map((val) => (val < 10 ? `0${val}` : val))
-            .filter((val, index) => val !== "00" || index > 0)
-            .join(":")
-            .replace(/^0/, "");
-    };
-    return (
-        <input type="text" onChange={onChange} onBlur={onBlur} value={value} />
-    );
-};
-
-export function ButtonsTimer() {
-    return (
-        <div>
-            <h4>Start</h4>
-            <TimeInput />
-            <h4>End</h4>
-            <TimeInput />
-        </div>
-    );
+    }
 }
 
 export default ButtonsTimer;
