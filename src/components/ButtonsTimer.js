4b693cbe3441f129888772ad56c48362f95a094f
Reyden <martin.leblancs@epitech.eu>
2022-07-07 15:28:40 +0200

    [ADD] ButtonTimer.js | Set the value in seconds

--- src/components/ButtonsTimer.js
+++ src/components/ButtonsTimer.js
@@ -1,32 +1,57 @@
 import * as React from 'react';
 import TimeField from 'react-simple-timefield';
 import './ButtonsTimer.css'
-import {TextField} from "@mui/material";
 
 class ButtonsTimer extends React.Component{
     constructor() {
         super();
 
         this.state = {
-            ValueStart: '00:00:00',
-            ValueEnd: '00:00:00',
+            ValueStart: '0',
+            ValueEnd: '0',
         };
 
         this.onTimeChange1 = this.onTimeChange1.bind(this);
         this.onTimeChange2 = this.onTimeChange2.bind(this);
     }
 
+    getSecondsFromHHMMSS = (value) => {
+        const [str1, str2, str3] = value.split(":");
+
+        const val1 = Number(str1);
+        const val2 = Number(str2);
+        const val3 = Number(str3);
+
+        if (!isNaN(val1) && isNaN(val2) && isNaN(val3)) {
+            // seconds
+            return val1;
+        }
+
+        if (!isNaN(val1) && !isNaN(val2) && isNaN(val3)) {
+            // minutes * 60 + seconds
+            return val1 * 60 + val2;
+        }
+
+        if (!isNaN(val1) && !isNaN(val2) && !isNaN(val3)) {
+            // hours * 60 * 60 + minutes * 60 + seconds
+            return val1 * 60 * 60 + val2 * 60 + val3;
+        }
+
+        return 0;
+    }
+    // For ValueStart
     onTimeChange1(event, value) {
         const newTime1 = value.replace(/-/g, ':');
         const ValueStart = newTime1.padEnd(8, this.state.ValueStart.substr(5, 3));
-
-        this.setState({ValueStart});
+        const result1 = this.getSecondsFromHHMMSS(ValueStart);
+        this.setState({result1});
     }
+    // For ValueEnd
     onTimeChange2(event, value) {
         const newTime2 = value.replace(/-/g, ':');
         const ValueEnd = newTime2.padEnd(8, this.state.ValueEnd.substr(5, 3));
-
-        this.setState({ValueEnd});
+        const result2 = this.getSecondsFromHHMMSS(ValueEnd);
+        this.setState({result2});
     }
     render() {
         const {ValueStart, ValueEnd} = this.state;
@@ -48,7 +73,7 @@ class ButtonsTimer extends React.Component{
                             color: '#333',
                             borderRadius: 3,
                             position: "absolute",
-                            left: 1000,
+                            left: 50,
                             top: 120
                         }}
                     />
@@ -69,8 +94,9 @@ class ButtonsTimer extends React.Component{
                             color: '#333',
                             borderRadius: 3,
                             position: "absolute",
-                            left: 50,
+                            left: 1000,
                             top: 120
+
                         }}
                     />
                 </section>
@@ -79,9 +105,4 @@ class ButtonsTimer extends React.Component{
     }
 }
 
-function mapStateToProps(state) {
-    const { start } = state
-    return { todoList: todos.allIds }
-}
-
-export default connect(mapStateToProps)(ButtonsTimer)
+export default ButtonsTimer;
